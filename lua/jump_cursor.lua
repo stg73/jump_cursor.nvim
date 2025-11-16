a5e53300d6c09e41013c1266f6606d530c0717f3
STG <stg737467@gmail.com>
2025-11-16 16:24:23 +0900

    docs: コメントを修正

--- lua/jump_cursor.lua
+++ lua/jump_cursor.lua
@@ -2,7 +2,6 @@ local M = {}
 
 local r = require("regex")
 
--- table -> table
 function M.opt(opts)
     -- オプション
     local opts = opts or {}
@@ -13,15 +12,13 @@ function M.opt(opts)
     local mark_table = vim.split(marks,"")
     local name_space = vim.api.nvim_create_namespace("jump_cursor")
 
-    local N = {}
+    local N = {} -- "M" の次の文字
 
-    -- ジャンプできる行を取得
-    -- string[] -> integer[]
     function N.get_jumpable_line(lines)
         local t = {}
 
         local function loop(i)
-            if not r.is("(" .. ignore .. ")*")(lines[i]) then -- ジャンプする行であれば
+            if not r.is("(" .. ignore .. ")*")(lines[i]) then -- ジャンプできる列がある行であれば
                 table.insert(t,i)
             end
 
@@ -35,22 +32,20 @@ function M.opt(opts)
         return t
     end
 
-    -- ジャンプできる列を取得
-    -- string -> integer[]
     -- nvim_buf_set_extmark()はバイト指定なのでそれに合わせてバイト数で取得する
     function N.get_jumpable_column(text)
         local t = {}
 
         local function loop(byte,str)
-            -- string.find だとマルチバイトに対応できない
+            -- string.find はマルチバイトに対応していない
             local s,e = r.find(".")(str)
             local char = string.sub(str,s,e)
             local rest = string.sub(str,e + 1)
 
             local char_column = byte + 1
-            local char_byte = string.len(char) -- マルチバイトに対応するためバイト数を取得する
+            local char_byte = string.len(char) -- バイト数を取得する
 
-            if not r.is(ignore)(char) then -- 無視する文字でなければ
+            if not r.is(ignore)(char) then -- ジャンプできる文字であれば
                 table.insert(t,char_column)
             end
 
@@ -64,8 +59,6 @@ function M.opt(opts)
         return t
     end
 
-    -- 特定の行を列別に塗り潰し 文字が入力されたらそれに対応する列を返す
-    -- integer -> integer
     function N.select_column(buf,line)
         local jumpable = N.get_jumpable_column(vim.api.nvim_buf_get_lines(buf,line - 1,line,false)[1])
         local function loop(i)
@@ -92,8 +85,6 @@ function M.opt(opts)
         return column
     end
 
-    -- 特定の範囲を行別に塗り潰し 文字が入力されたらそれに対応する行を返す
-    -- integer, integer -> integer
     function N.select_line(buf,s,e)
         local lines = vim.api.nvim_buf_get_lines(buf,s - 1,e,false)
         local jumpable_line = N.get_jumpable_line(lines)
@@ -112,14 +103,14 @@ function M.opt(opts)
                 end
             end
 
-            loop_column(1)
+            loop_column(1) -- 行をハイライトする
 
             if jumpable_line[l + 1] and mark_table[l + 1] then
                 loop_line(l + 1)
             end
         end
 
-        loop_line(1)
+        loop_line(1) -- 複数行をハイライトする
         vim.cmd.redraw()
 
         local mark = vim.fn.getcharstr()
@@ -132,7 +123,6 @@ function M.opt(opts)
         end
     end
 
-    -- integer, integer -> integer[]
     function N.select_position(buf,s,e)
         local line = N.select_line(buf,s,e)
         if line then
@@ -147,7 +137,6 @@ function M.opt(opts)
         end
     end
 
-    -- 本体
     function N.jump()
         local pos = N.select_position(0,vim.fn.line("w0"),vim.fn.line("w$"))
         if pos then
